385f0b6b4ac1dd88b7eabc4006517eb040e42b15
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-18 18:03:51 -0400

    Changed validation logic of 29.py so that a function is no longer needed and so someone reading the code would easily be able to figure out that it's validating input.

--- exercises/29th_lesson/29.py
+++ exercises/29th_lesson/29.py
@@ -7,17 +7,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
 args = argv
-
-def usage_error():
+input_path = Path(args[1])
+write_path = Path(args[2])
+valid_input = len(args) == 3 and input_path.name.endswith(".md") and input_path.exists() and write_path.name.endswith(".json")
+if not valid_input:
     logging.critical("The correct format is python 29.py <input_markdown_file> <output_json_path>")
     exit(1)
 
-if len(args) != 3:
-    usage_error()
 
-input_path = Path(args[1])
-if not input_path.name.endswith(".md") or not input_path.exists():
-    usage_error()
 
 
 
