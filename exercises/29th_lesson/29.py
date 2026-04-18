1e05870884e6f5af5ae135cbe38ee4e08118d845
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-18 18:49:55 -0400

    Lesson 29. Successfully used regex to capture link text and URL and to create a list of dictionaries that contain this content.

--- exercises/29th_lesson/29.py
+++ exercises/29th_lesson/29.py
@@ -4,7 +4,7 @@ import re
 import json
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 args = argv
 input_path = Path(args[1])
@@ -13,6 +13,14 @@ valid_input = len(args) == 3 and input_path.name.endswith(".md") and input_path.
 if not valid_input:
     logging.critical("The correct format is python 29.py <input_markdown_file> <output_json_path>")
     exit(1)
+
+output = []
+input_text = input_path.read_text(encoding='utf-8')
+for match in re.finditer(r'\[(.*?)\]\((.*?)\)', input_text):
+    logging.debug(f"The link text is {match.group(1)}, and the URL is {match.group(2)}.")
+    output.append(dict(link_text = match.group(1), url = match.group(2)))
+
+print(output)
 
 
 
