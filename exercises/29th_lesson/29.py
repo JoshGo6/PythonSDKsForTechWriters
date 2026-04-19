72e57c7e76b9fc4c2ad209562c454b6f9ecfa517
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-18 20:16:04 -0400

    Added logic to test if outfile path contains a directory and if so, does the directory exist.

--- exercises/29th_lesson/29.py
+++ exercises/29th_lesson/29.py
@@ -6,6 +6,16 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
+def validate_write_path(write_path):
+    if "/" in str(write_path):
+        write_dir = str(write_path).rsplit("/", 1)[0]
+        print(write_dir)
+        if not Path(write_dir).exists():
+            print(f"The output directory doesn't exist.")
+            Path(write_dir).mkdir(parents=True, exist_ok=True)
+            print(f"Successfully created output directory.")
+
+
 args = argv
 input_path = Path(args[1])
 write_path = Path(args[2])
@@ -14,15 +24,24 @@ if not valid_input:
     logging.critical("The correct format is python 29.py <input_markdown_file> <output_json_path>")
     exit(1)
 
+write_path_contains_dir = "/" in str(write_path)
+if write_path_contains_dir:
+    validate_write_path(write_path)
+
 output = []
 input_text = input_path.read_text(encoding='utf-8')
 for match in re.finditer(r'\[(.*?)\]\((.*?)\)', input_text):
     logging.debug(f"The link text is {match.group(1)}, and the URL is {match.group(2)}.")
     output.append(dict(link_text = match.group(1), url = match.group(2)))
 
-print(output)
+try:
+    with open(write_path, 'w', encoding='utf-8') as f:
+        json.dump(output, f, indent=2, sort_keys=True)
+    print(f"Extracted {len(output)} links.")
+except FileNotFoundError as e:
+    logging.critical('Unable to write to file.')
 
-
+Path("../29th_test_dir").mkdir(parents=True, exist_ok=True)
 
 
 
