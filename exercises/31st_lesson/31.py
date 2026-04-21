e5afdf65c62156a23cc16399f37328c094b3b961
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-21 15:10:53 -0400

    Migrated all validation logic and loading of enviornment variables into a single validation function that returns the original enviornment variables and a processed form of them.

--- exercises/31st_lesson/31.py
+++ exercises/31st_lesson/31.py
@@ -7,7 +7,7 @@ import sys
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
-def validatate_input():
+def validate_input():
     try:
         app_token = os.environ["APP_TOKEN"]
     except KeyError:
@@ -17,24 +17,26 @@ def validatate_input():
         logging.error(f"The token is in the wrong format. It must start with one of the following prefixes:\n- {"\n- ".join(prefixes)}")
         sys.exit(2)
 
+    APP_TOKEN = os.environ["APP_TOKEN"]
+    APP_ORG = os.getenv("APP_ORG", "default_org")
+    APP_LOG_LEVEL = os.getenv("APP_LOG_LEVEL", "INFO")
+
+    processed_vars = {
+        "token_preview": APP_TOKEN[:4],
+        "org": APP_ORG,
+        "log_level": APP_LOG_LEVEL,
+        "variables_loaded": 3
+    }
+
+    return ((APP_TOKEN, APP_ORG, APP_LOG_LEVEL, processed_vars))
+
 parser = argparse.ArgumentParser(description="Loads three environment variables, shows a preview of APP_TOKEN, and shows the values of APP_ORG and APP_LOG_LEVEL.")
 parser.add_argument("--output", action="store_true", help="Path to output file.")
 args = parser.parse_args()
 
 prefixes = {"tok_", "key_"}
 
-validatate_input()
+processed_vars = validate_input()[3]
 
-APP_TOKEN = os.environ["APP_TOKEN"]
-APP_ORG = os.getenv("APP_ORG", "default_org")
-APP_LOG_LEVEL = os.getenv("APP_LOG_LEVEL", "INFO")
-
-environ_vars = {
-    "token_preview": APP_TOKEN[:4],
-    "org": APP_ORG,
-    "log_level": APP_LOG_LEVEL,
-    "variables_loaded": 3
-}
-
-print(environ_vars)
+print(processed_vars)
 
