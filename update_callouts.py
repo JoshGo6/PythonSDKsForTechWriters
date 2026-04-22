0c71cc0e492303aafe975fdc658771a2743df257
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-22 01:43:04 -0400

    Updates to update_callouts.py

--- update_callouts.py
+++ update_callouts.py
@@ -9,9 +9,6 @@ import re
 for md_file in Path('.').glob("*.md"):
     with open(md_file, 'r', encoding='utf-8') as f:
         original_content = f.read()
-    updated_content = re.sub(r'(> \[!(?:note|tip|important|warning|caution)\]) ', r'\1\n> ', original_content)
-    #with open(md_file, 'w', encoding='utf-8') as f:
- #   updated_content = re.sub(r'^(> \[!(?:tip|note|important|info|warning)\]) ', r'\1\n> ', original_content)
-    with open(Path("temp.md"), 'a', encoding='utf-8') as f:
+    updated_content = re.sub(r'(> \[!(?:note|tip|important|warning|caution)\])\s+(?!\n)', r'\1\n> ', original_content)
+    with open(md_file, 'w', encoding='utf-8') as f:
         f.write(updated_content)
-
