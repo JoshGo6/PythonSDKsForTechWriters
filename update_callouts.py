7e02129301fac98a4a67169afa56ecb13843decb
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-23 17:09:09 -0400

    Modified update_callouts.py so it wouldn't target the lesson plan doc when the script runs.

--- update_callouts.py
+++ update_callouts.py
@@ -7,6 +7,8 @@ import re
 
 
 for md_file in Path('.').glob("*.md"):
+    if md_file.name == "Python Lesson Plan for SDKs.md":
+        continue
     with open(md_file, 'r', encoding='utf-8') as f:
         original_content = f.read()
     updated_content = re.sub(r'(> \[!(?:note|tip|important|warning|caution)\] )( )*(?!\n)', r'\1\n> ', original_content, flags=re.MULTILINE)
