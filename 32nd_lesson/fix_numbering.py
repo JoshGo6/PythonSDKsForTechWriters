2290c6a8d17cbdb332cf1c9590e20dbfe96a2c1a
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-24 23:25:32 -0400

    Began function for updating lines. Also created the logic for writing the updated content to the file.

--- 32nd_lesson/fix_numbering.py
+++ 32nd_lesson/fix_numbering.py
@@ -9,6 +9,10 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
     
+def updated_line(content):
+    match_text = re.match(r'^#(.*?)(\d+)(.*?)', content)
+    if not match_text:
+        return content
 
 parser = argparse.ArgumentParser(description="Increment the integer in headings that occur beyond a certain point in a Markdown document.")
 parser.add_argument("path", help="Path to input file.")
@@ -18,6 +22,7 @@ args=parser.parse_args()
 
 input_file = Path(args.path)
 search_text = args.start_text
+unaffected_lines = []
 
 if not input_file.exists():
     logging.error("The file doesn't exist.")
@@ -29,10 +34,18 @@ with open(input_file, 'r', encoding='utf-8') as f:
         logging.debug(f"{line_num}: {content}")
         if re.findall(r'^```', content):
             in_code_block = not in_code_block
+            unaffected_lines.append(content)
             continue
         elif in_code_block:
+            unaffected_lines.append(content)
             continue
         elif re.findall(rf'^#.*?{search_text}', content):
             replacement_line_start = line_num
             break
-        
+
+with open(input_file, 'w', encoding='utf-8') as f:
+    for line_num, content in enumerate(f):
+        if line_num < replacement_line_start:
+            f.write(unaffected_lines[line_num])
+        else:
+            f.write(updated_line(content))
