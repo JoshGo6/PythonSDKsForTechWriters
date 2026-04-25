51f68eb3d1ad394bcb2681989f2bda83b64dcd96
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-25 01:28:47 -0400

    Completed script for fixing numbering of lesson artifact.

--- 32nd_lesson/fix_numbering.py
+++ 32nd_lesson/fix_numbering.py
@@ -9,10 +9,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
     
-def updated_line(content):
-    match_text = re.match(r'^#(.*?)(\d+)(.*?)', content)
+def update(content):
+    match_text = re.match(r'^(#+ Lesson )(\d+)(.*?)', content)
     if not match_text:
         return content
+    else:
+        old_num = match_text.group(2)
+        new_num = str(int(old_num) + 1)
+        logging.info(f"Old num: {old_num}. New num: {new_num}.")
+        return(re.sub(old_num, new_num, content))
 
 parser = argparse.ArgumentParser(description="Increment the integer in headings that occur beyond a certain point in a Markdown document.")
 parser.add_argument("path", help="Path to input file.")
@@ -23,29 +28,38 @@ args=parser.parse_args()
 input_file = Path(args.path)
 search_text = args.start_text
 unaffected_lines = []
+replacement_lines = []
 
 if not input_file.exists():
     logging.error("The file doesn't exist.")
     sys.exit(1)
 
 with open(input_file, 'r', encoding='utf-8') as f:
+    numbered_lines = list(enumerate(f))
     in_code_block = False
-    for line_num, content in enumerate(f):
+    for line_num, content in numbered_lines:
+        unaffected_lines.append(content)
         logging.debug(f"{line_num}: {content}")
         if re.findall(r'^```', content):
             in_code_block = not in_code_block
-            unaffected_lines.append(content)
             continue
         elif in_code_block:
-            unaffected_lines.append(content)
             continue
         elif re.findall(rf'^#.*?{search_text}', content):
             replacement_line_start = line_num
+            unaffected_lines.pop()
             break
+    for line_num, content in numbered_lines:
+        if line_num >= replacement_line_start:
+            replacement_lines.append(update(content))
+
+logging.info(f"Line replacement begins with the following line: '{replacement_line_start}'")
+logging.info(f"The unaffected_lines list contains {len(unaffected_lines)} lines.")
+logging.info(f"The replacement_lines list contains {len(replacement_lines)} lines.")
 
 with open(input_file, 'w', encoding='utf-8') as f:
-    for line_num, content in enumerate(f):
-        if line_num < replacement_line_start:
-            f.write(unaffected_lines[line_num])
-        else:
-            f.write(updated_line(content))
+    for line in unaffected_lines:
+        f.write(line)
+    for line in replacement_lines:
+        f.write(line)
+    
