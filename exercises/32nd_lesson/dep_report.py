f8daaeb0cd7f4e7603422426295c9cfbb5681e34
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-26 01:39:25 -0400

    Moved some files for the 32nd lesson and added some new ones.

---
+++ exercises/32nd_lesson/dep_report.py
@@ -0,0 +1,8 @@
+import json
+from tabulate import tabulate
+from pathlib import Path
+import argparse
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+
--- update_callouts.py
+++ update_callouts.py
@@ -11,6 +11,6 @@ for md_file in Path('.').glob("*.md"):
         continue
     with open(md_file, 'r', encoding='utf-8') as f:
         original_content = f.read()
-    updated_content = re.sub(r'(> \[!(?:note|tip|important|warning|caution)\] )( )*(?!\n)', r'\1\n> ', original_content, flags=re.MULTILINE)
+    updated_content = re.sub(r'(> \[!(?:note|tip|important|warning|caution|info)\] )( )*(?!\n)', r'\1\n> ', original_content, flags=re.MULTILINE)
     with open(md_file, 'w', encoding='utf-8') as f:
         f.write(updated_content)
