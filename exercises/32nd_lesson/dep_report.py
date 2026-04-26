663b0c8ae4c1c7f8849583982c6cb76ac2c9babe
Josh Goldstein <joshgoldstein@joshgoldstein.org>
2026-04-26 02:46:21 -0400

    32nd lesson. argparse object done for dep_report.py. Able to validate and load input file successfully and to output it back again to JSON.

--- exercises/32nd_lesson/dep_report.py
+++ exercises/32nd_lesson/dep_report.py
@@ -6,3 +6,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
 
+parser = argparse.ArgumentParser(description="Outputs to the CLI tabulated data from a JSON file.")
+parser.add_argument("json_path", help="Path to the input JSON file.")
+parser.add_argument("--category", help="Filter the output to data matching this value of the 'category' key.")
+cli_args = parser.parse_args()
+
+try:
+    with open(Path(cli_args.json_path), "r", encoding="utf-8") as f:
+        data = json.load(f)
+except FileNotFoundError as e:
+    logging.error(f"File {cli_args.json_path} does not exist.")
+
+data.sort(key=lambda dictionary:dictionary["category"])
+logging.debug(json.dumps(data, indent=2, sort_keys=True))
